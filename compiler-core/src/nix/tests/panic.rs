f7ced88b9603518c136e937ee68c7db613043ca3
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-03-25 04:45:58 -0300

    add panic tests

---
+++ compiler-core/src/nix/tests/panic.rs
@@ -0,0 +1,72 @@
+use crate::assert_nix;
+
+#[test]
+fn bare() {
+    assert_nix!(
+        r#"
+fn go() {
+  panic
+}
+"#,
+    );
+}
+
+#[test]
+fn panic_as() {
+    assert_nix!(
+        r#"
+fn go() {
+  let x = "wibble"
+  panic as x
+}
+"#,
+    );
+}
+
+#[test]
+fn as_expression() {
+    assert_nix!(
+        r#"
+fn go(f) {
+  let boop = panic
+  f(panic)
+}
+"#,
+    );
+}
+
+#[test]
+fn pipe() {
+    assert_nix!(
+        r#"
+fn go(f) {
+  f |> panic
+}
+"#,
+    );
+}
+
+#[test]
+fn sequence() {
+    assert_nix!(
+        r#"
+fn go(at_the_disco) {
+  panic
+  at_the_disco
+}
+"#,
+    );
+}
+
+#[test]
+fn case() {
+    assert_nix!(
+        r#"
+fn go(x) {
+  case x {
+    _ -> panic
+  }
+}
+"#,
+    );
+}
