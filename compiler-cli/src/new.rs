59c04e86b57a31281ef52e22e6df4f69765b5126
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-04-22 20:06:50 -0300

    cli/new: update README

--- compiler-cli/src/new.rs
+++ compiler-cli/src/new.rs
@@ -95,7 +95,7 @@ impl FileToCreate {
 [![Hex Docs](https://img.shields.io/badge/hex-docs-ffaff3)](https://hexdocs.pm/{project_name}/)
 
 ```sh
-gleam add {project_name}
+glistix add {project_name}
 ```
 ```gleam
 import {project_name}
@@ -105,14 +105,34 @@ pub fn main() {{
 }}
 ```
 
+**Note:** This is a Glistix project, and as such may require the
+[Glistix compiler](https://github.com/glistix/glistix) to be used.
+
 Further documentation can be found at <https://hexdocs.pm/{project_name}>.
+
+## Importing from Nix
+
+To import this project from Nix, first fetch its source (through a Flake input,
+using `builtins.fetchGit`, cloning to a local path, or some other way), import
+the Flake or the `default.nix` file, and run `lib.loadGlistixPackage {{ }}`.
+For example:
+
+```nix
+let
+  # Assuming the project was cloned to './path/to/project'
+  {project_name} = import ./path/to/project;
+  # Use 'loadGlistixPackage {{ module = "module/name"; }}' to pick a module
+  package = {project_name}.lib.loadGlistixPackage {{ }};
+  result = package.main {{ }};
+in result
+```
 
 ## Development
 
 ```sh
-gleam run   # Run the project
-gleam test  # Run the tests
-gleam shell # Run an Erlang shell
+nix develop   # Optional: Spawn a shell with glistix
+glistix run   # Run the project
+glistix test  # Run the tests
 ```
 "#,
             )),
