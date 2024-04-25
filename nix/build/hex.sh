61d02b27bf91cc5f7119fb116e2afb5e6f42b7b0
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-04-24 23:09:34 -0300

    nix/build: use mktemp -d on hex.sh

--- nix/build/hex.sh
+++ nix/build/hex.sh
@@ -1,5 +1,5 @@
 # Populates Gleam's cache of hex packages.
-export XDG_CACHE_HOME="$TEMPDIR/.gleam_cache"
+export XDG_CACHE_HOME="$(mktemp -d)/.gleam_cache"
 hexdir="${XDG_CACHE_HOME}/gleam/hex/hexpm/packages"
 mkdir -p "$hexdir"
 
