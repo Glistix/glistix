gleam-lang/gleam

Add arm docker image

containers/elixir-alpine.dockerfile
@@ -1,3 +1,6 @@
 FROM elixir:alpine
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

containers/elixir-slim.dockerfile
@@ -1,3 +1,6 @@
 FROM elixir:slim
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

containers/elixir.dockerfile
@@ -1,3 +1,6 @@
 FROM elixir:latest
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

containers/erlang-slim.dockerfile
@@ -1,3 +1,6 @@
 FROM erlang:slim
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

containers/erlang.dockerfile
@@ -1,3 +1,6 @@
 FROM erlang:latest
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

containers/node-alpine.dockerfile
@@ -1,3 +1,6 @@
 FROM node:alpine
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

containers/node-slim.dockerfile
@@ -1,3 +1,6 @@
 FROM node:slim
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

containers/node.dockerfile
@@ -1,3 +1,6 @@
 FROM node:latest
-COPY gleam /bin
+
+ARG TARGETARCH
+COPY gleam-${TARGETARCH} /bin/gleam
+
 CMD ["gleam"]

Glistix/glistix

containers/elixir-alpine.dockerfile
@@ -1,3 +1,6 @@
 FROM elixir:alpine
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]

containers/elixir-slim.dockerfile
@@ -1,3 +1,6 @@
 FROM elixir:slim
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]

containers/elixir.dockerfile
@@ -1,3 +1,6 @@
 FROM elixir:latest
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]

containers/erlang-slim.dockerfile
@@ -1,3 +1,6 @@
 FROM erlang:slim
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]

containers/erlang.dockerfile
@@ -1,3 +1,6 @@
 FROM erlang:latest
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]

containers/node-alpine.dockerfile
@@ -1,3 +1,6 @@
 FROM node:alpine
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]

containers/node-slim.dockerfile
@@ -1,3 +1,6 @@
 FROM node:slim
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]

containers/node.dockerfile
@@ -1,3 +1,6 @@
 FROM node:latest
-COPY glistix /bin
+
+ARG TARGETARCH
+COPY glistix-${TARGETARCH} /bin/glistix
+
 CMD ["glistix"]