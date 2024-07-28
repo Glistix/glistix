c52ad8fa5f56c0a9d49fd2d230cd09baddd6be5f
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-07-28 02:36:40 -0300

    fix comment in nix ffi

--- test/language/test/ffi_nix.nix
+++ test/language/test/ffi_nix.nix
@@ -6,7 +6,7 @@ let
     f:
         let
             # ./. is relative to this file in the build directory (language/build/dev/nix/ffi_nix.nix)
-            # In other targets, this would be relative to 'language/', so let's go back there'
+            # In other targets, this would be relative to 'language/', so let's go back there
             absPath = if builtins.isString f && builtins.substring 0 1 f != "/" then ./../../../../${f} else f;
         in builtins.pathExists absPath;
   halt = code: if code == 0 then null else builtins.abort (toString code);
