2354988e22d80abfe515d45f8a72ce20be139a42
GearsDatapacks <surya@seriousinfinitude.com>
2024-09-10 13:20:09 +0100

    Merge `TypeNames` and `ValueNames` structs

--- compiler-core/src/analyse.rs
+++ compiler-core/src/analyse.rs
@@ -298,7 +298,7 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
             module_types_constructors: types_constructors,
             module_values: values,
             accessors,
-            type_names,
+            names: type_names,
             ..
         } = env;
 
@@ -1099,7 +1099,7 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
                 deprecation.clone(),
             );
 
-            environment.value_names.named_constructor_in_scope(
+            environment.names.named_constructor_in_scope(
                 environment.current_module.clone(),
                 constructor.name.clone(),
                 constructor.name.clone(),
@@ -1186,7 +1186,7 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
             )
             .expect("name uniqueness checked above");
 
-        environment.type_names.named_type_in_scope(
+        environment.names.named_type_in_scope(
             environment.current_module.clone(),
             name.clone(),
             name.clone(),
@@ -1246,7 +1246,7 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
                 ..
             } = type_.as_ref()
             {
-                environment.type_names.named_type_in_scope(
+                environment.names.named_type_in_scope(
                     module.clone(),
                     type_name.clone(),
                     name.clone(),
