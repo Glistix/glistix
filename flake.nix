ebb3ab5e55165ea5e422e0bfc0404ecbaf5bec6d
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-10-28 23:56:12 -0300

    switch to fenix

--- flake.nix
+++ flake.nix
@@ -2,9 +2,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-manifest = {
+      url = "https://static.rust-lang.org/dist/channel-rust-1.81.0.toml";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, systems, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, systems, fenix, rust-manifest, ... }:
     let
       expressions = import ./nix;
     in
@@ -20,9 +28,16 @@
         ./nix/flake-builders.nix
       ];
 
-      perSystem = { self', pkgs, lib, ... }:
+      perSystem = { self', pkgs, lib, system, ... }:
         let
-          glistix = pkgs.callPackage expressions.glistix { };
+          rust-toolchain = (fenix.packages.${system}.fromManifestFile rust-manifest).defaultToolchain;
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rust-toolchain;
+            rustc = rust-toolchain;
+          };
+
+          glistix = pkgs.callPackage expressions.glistix { inherit rustPlatform; };
         in
         {
           formatter = pkgs.nixpkgs-fmt;
