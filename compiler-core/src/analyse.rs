GearsDatapacks/gleam

Change "import module" code action to read from diagnostics, Add hint to import modules when a module exists with the same name

compiler-core/src/analyse.rs
@@ -290,7 +290,6 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
             module_types_constructors: types_constructors,
             module_values: values,
             accessors,
-            import_suggestions,
             ..
         } = env;
 
@@ -801,7 +800,7 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
             )
             .collect();
         let typed_parameters = environment
-            .get_type_constructor(&None, &name, location)
+            .get_type_constructor(&None, &name)
             .expect("Could not find preregistered type constructor ")
             .parameters
             .clone();
@@ -1324,7 +1323,7 @@ fn analyse_type_alias(t: TypeAlias<()>, environment: &mut Environment<'_>) -> Ty
     // analysis aims to be fault tolerant to get the best possible feedback for
     // the programmer in the language server, so the analyser gets here even
     // though there was previously errors.
-    let typ = match environment.get_type_constructor(&None, &alias, location) {
+    let typ = match environment.get_type_constructor(&None, &alias) {
         Ok(constructor) => constructor.typ.clone(),
         Err(_) => environment.new_generic_var(),
     };
@@ -1700,33 +1699,4 @@ fn sorted_type_aliases(aliases: &Vec<TypeAlias<()>>) -> Result<Vec<&TypeAlias<()
         .iter()
         .sorted_by_key(|alias| sorted_deps.iter().position(|x| x == &alias.alias))
         .collect())
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ImportSuggestion {
-    pub location: SrcSpan,
-    pub suggestion: EcoString,
-}
-
-pub fn suggest_imports(
-    module_name: &str,
-    location: SrcSpan,
-    importable_modules: &[&EcoString],
-    import_suggestions: &mut Vec<ImportSuggestion>,
-) {
-    import_suggestions.extend(importable_modules.iter().filter_map(|&option| {
-        if option
-            .split('/')
-            .last()
-            .unwrap_or(option)
-            .eq_ignore_ascii_case(module_name)
-        {
-            Some(ImportSuggestion {
-                location,
-                suggestion: option.clone(),
-            })
-        } else {
-            None
-        }
-    }));
 }

Glistix/glistix

compiler-core/src/analyse.rs
@@ -292,7 +292,6 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
             module_types_constructors: types_constructors,
             module_values: values,
             accessors,
-            import_suggestions,
             ..
         } = env;
 
@@ -859,7 +858,7 @@ impl<'a, A> ModuleAnalyzer<'a, A> {
             )
             .collect();
         let typed_parameters = environment
-            .get_type_constructor(&None, &name, location)
+            .get_type_constructor(&None, &name)
             .expect("Could not find preregistered type constructor ")
             .parameters
             .clone();
@@ -1402,7 +1401,7 @@ fn analyse_type_alias(t: UntypedTypeAlias, environment: &mut Environment<'_>) ->
     // analysis aims to be fault tolerant to get the best possible feedback for
     // the programmer in the language server, so the analyser gets here even
     // though there was previously errors.
-    let type_ = match environment.get_type_constructor(&None, &alias, location) {
+    let type_ = match environment.get_type_constructor(&None, &alias) {
         Ok(constructor) => constructor.type_.clone(),
         Err(_) => environment.new_generic_var(),
     };
@@ -1784,33 +1783,4 @@ fn sorted_type_aliases(aliases: &Vec<UntypedTypeAlias>) -> Result<Vec<&UntypedTy
         .iter()
         .sorted_by_key(|alias| sorted_deps.iter().position(|x| x == &alias.alias))
         .collect())
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ImportSuggestion {
-    pub location: SrcSpan,
-    pub suggestion: EcoString,
-}
-
-pub fn suggest_imports(
-    module_name: &str,
-    location: SrcSpan,
-    importable_modules: &[&EcoString],
-    import_suggestions: &mut Vec<ImportSuggestion>,
-) {
-    import_suggestions.extend(importable_modules.iter().filter_map(|&option| {
-        if option
-            .split('/')
-            .last()
-            .unwrap_or(option)
-            .eq_ignore_ascii_case(module_name)
-        {
-            Some(ImportSuggestion {
-                location,
-                suggestion: option.clone(),
-            })
-        } else {
-            None
-        }
-    }));
 }