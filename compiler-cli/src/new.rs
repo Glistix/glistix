c56751f6513e0396adb9e248621332771447ca09
Richard Viney <richard.viney@gmail.com>
2024-11-13 12:57:07 +1300

    Specify Erlang v27 in the CI workflow for new projects

--- compiler-cli/src/new.rs
+++ compiler-cli/src/new.rs
@@ -18,7 +18,7 @@ use crate::{fs::get_current_directory, NewOptions};
 const GLEAM_STDLIB_REQUIREMENT: &str = ">= 0.34.0 and < 2.0.0";
 const GLEEUNIT_REQUIREMENT: &str = ">= 1.0.0 and < 2.0.0";
 #[allow(dead_code)]
-const ERLANG_OTP_VERSION: &str = "26.0.2";
+const ERLANG_OTP_VERSION: &str = "27.1.2";
 #[allow(dead_code)]
 const REBAR3_VERSION: &str = "3";
 #[allow(dead_code)]
