56153285e81ec29ed022a38bc523327067ba6782
Jason Sipula <jason@fluximus.com>
2024-11-18 22:44:01 -0800

    fix update use_manifest logic

--- compiler-cli/src/dependencies.rs
+++ compiler-cli/src/dependencies.rs
@@ -116,7 +116,7 @@ pub enum UseManifest {
 
 pub fn update(packages: Vec<String>) -> Result<()> {
     let paths = crate::find_project_paths()?;
-    let use_manifest = if packages.is_empty() {
+    let use_manifest = if !packages.is_empty() {
         UseManifest::Yes
     } else {
         UseManifest::No
