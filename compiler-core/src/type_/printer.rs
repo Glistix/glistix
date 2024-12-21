gleam-lang/gleam

Fix clippy warnings for 1.83

compiler-core/src/type_/printer.rs
@@ -210,7 +210,7 @@ impl Names {
             return NameContextInformation::Qualified(module, name.as_str());
         };
 
-        return NameContextInformation::Unimported(name.as_str());
+        NameContextInformation::Unimported(name.as_str())
     }
 
     /// Record a named value in this module.

Glistix/glistix

compiler-core/src/type_/printer.rs
@@ -148,7 +148,7 @@ impl TypeNames {
             return NamedTypeNames::Qualified(module, name.as_str());
         };
 
-        return NamedTypeNames::Unimported(name.as_str());
+        NamedTypeNames::Unimported(name.as_str())
     }
 
     /// A suitable name of a type variable.