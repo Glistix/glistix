ee0f9571bca86b5a1cceb0c4b1f5bac2d309dd4f
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-12-21 17:18:52 -0300

    Bump flake to Rust 1.83.0 (#28)
    * bump flake to Rust 1.83.0
    
    * is_none_or
    
    ---------
    
    Co-authored-by: GearsDatapacks <surya@seriousinfinitude.com>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     rust-manifest = {
-      url = "https://static.rust-lang.org/dist/channel-rust-1.81.0.toml";
+      url = "https://static.rust-lang.org/dist/channel-rust-1.83.0.toml";
       flake = false;
     };
   };
