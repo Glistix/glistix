754e063d459061054f7ebedefa37471f2f04d045
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-12-23 20:46:52 -0300

    fix inconsistency between manifest and compiled package config
    During compilation, dependencies' configs must be updated, not only in
    the CLI side when generating the manifest or reading the root config.
    
    Otherwise, we'd get a warning saying that the dependency is importing a
    module from something that it doesn't depend on, which it can only
    suppose to be a transitive dependency.

--- compiler-core/src/build/project_compiler.rs
+++ compiler-core/src/build/project_compiler.rs
@@ -514,7 +514,15 @@ where
             }
         };
         let config_path = package_root.join("gleam.toml");
-        let config = PackageConfig::read(config_path, &self.io)?;
+        let config = PackageConfig::read(config_path, &self.io).map(|mut c| {
+            // Apply root config's replacements to dependency
+            self.config
+                .glistix
+                .preview
+                .replacements
+                .patch_config(&mut c, false);
+            c
+        })?;
         self.compile_gleam_package(&config, false, package_root)
             .into_result()
     }
