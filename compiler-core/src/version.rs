3d7477fd4787a2df4cdf937709fb243af1682e9e
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-12-28 01:07:46 -0300

    update compiler version

--- compiler-core/src/version.rs
+++ compiler-core/src/version.rs
@@ -3,4 +3,4 @@
 /// instead build from scratch
 /// Note that this should be updated to correspond to the Gleam version
 /// we are basing Glistix on. This is checked by packages.
-pub const COMPILER_VERSION: &str = "1.6.0";
+pub const COMPILER_VERSION: &str = "1.6.3";
