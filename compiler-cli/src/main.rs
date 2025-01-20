7c077fc85d1afc132a290e4f549d74c2abbbfe24
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-01-20 14:15:13 -0300

    write and export nix prelude

--- compiler-cli/src/main.rs
+++ compiler-cli/src/main.rs
@@ -282,6 +282,8 @@ pub enum ExportTarget {
     JavascriptPrelude,
     /// The TypeScript prelude module
     TypescriptPrelude,
+    /// The Nix prelude module
+    NixPrelude,
     /// Information on the modules, functions, and types in the project in JSON format
     PackageInterface {
         #[arg(long = "out", required = true)]
@@ -551,6 +553,7 @@ fn main() {
         Command::Export(ExportTarget::HexTarball) => export::hex_tarball(),
         Command::Export(ExportTarget::JavascriptPrelude) => export::javascript_prelude(),
         Command::Export(ExportTarget::TypescriptPrelude) => export::typescript_prelude(),
+        Command::Export(ExportTarget::NixPrelude) => export::nix_prelude(),
         Command::Export(ExportTarget::PackageInterface { output }) => {
             export::package_interface(output)
         }
