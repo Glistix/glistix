8df28476f72214400d159d3463caec2eccbd1357
PgBiel <9021226+PgBiel@users.noreply.github.com>
2025-01-20 14:15:13 -0300

    cli/new: add flake.nix with workflow, target=nix, patching
    recommend depending on glistix_stdlib by default: see 2a2630671b3679a18f43ad618d55056636d8897a
    
    add book links: see f0f0df37d3b9cfddb63dba9010f49a72eaa33889
    
    change 1: see aeb3b9fd0f95b19e382a84c10b0dd9942855bf2c
    (gh:glistix#44) and 408dfdfa3fad9cdf25d88ba9c693912c8a1ebce0 (gh:glistix#48)
    
    initial: see 64710af5e29922a209100f6f221a480a9a114236,
    c7b59c23262fa2477bca3e8a4f6d8c6fc2ba7ce6,
    a3083280e2d89861379dcaa90d86b3c8e3814a7e,
    1b081917ab42330165c501db5a075c74d82eb5fc and related commits

--- compiler-cli/src/new/tests.rs
+++ compiler-cli/src/new/tests.rs
@@ -11,7 +11,7 @@ fn new() {
     let creator = super::Creator::new(
         super::NewOptions {
             project_root: path.to_string(),
-            template: super::Template::Erlang,
+            template: super::Template::Nix,
             name: None,
             skip_git: false,
             skip_github: false,
@@ -29,6 +29,10 @@ fn new() {
     assert!(path.join("test/my_project_test.gleam").exists());
     assert!(path.join(".github/workflows/test.yml").exists());
 
+    assert!(path.join("flake.nix").exists());
+    assert!(path.join("default.nix").exists());
+    assert!(path.join("shell.nix").exists());
+
     let toml = crate::fs::read(path.join("gleam.toml")).unwrap();
     assert!(toml.contains("name = \"my_project\""));
 }
@@ -41,7 +45,7 @@ fn new_with_default_template() {
     let creator = super::Creator::new(
         super::NewOptions {
             project_root: path.join("my_project").to_string(),
-            template: super::Template::Erlang,
+            template: super::Template::Nix,
             name: None,
             skip_git: false,
             skip_github: true,
@@ -101,7 +105,7 @@ fn new_with_skip_git() {
     let creator = super::Creator::new(
         super::NewOptions {
             project_root: path.to_string(),
-            template: super::Template::Erlang,
+            template: super::Template::Nix,
             name: None,
             skip_git: true,
             skip_github: false,
@@ -122,7 +126,7 @@ fn new_with_skip_github() {
     let creator = super::Creator::new(
         super::NewOptions {
             project_root: path.to_string(),
-            template: super::Template::Erlang,
+            template: super::Template::Nix,
             name: None,
             skip_git: false,
             skip_github: true,
@@ -146,7 +150,7 @@ fn new_with_skip_git_and_github() {
     let creator = super::Creator::new(
         super::NewOptions {
             project_root: path.to_string(),
-            template: super::Template::Erlang,
+            template: super::Template::Nix,
             name: None,
             skip_git: true,
             skip_github: true,
@@ -171,7 +175,7 @@ fn invalid_path() {
         super::Creator::new(
             super::NewOptions {
                 project_root: path.to_string(),
-                template: super::Template::Erlang,
+                template: super::Template::Nix,
                 name: None,
                 skip_git: false,
                 skip_github: false,
@@ -191,7 +195,7 @@ fn invalid_name() {
         super::Creator::new(
             super::NewOptions {
                 project_root: path.to_string(),
-                template: super::Template::Erlang,
+                template: super::Template::Nix,
                 name: Some("-".into()),
                 skip_git: false,
                 skip_github: false,
@@ -212,7 +216,7 @@ fn existing_directory_no_files() {
     let creator = super::Creator::new(
         super::NewOptions {
             project_root: path.to_string(),
-            template: super::Template::Erlang,
+            template: super::Template::Nix,
             name: None,
             skip_git: true,
             skip_github: true,
@@ -240,7 +244,7 @@ fn existing_directory_with_one_existing_file() {
         super::Creator::new(
             super::NewOptions {
                 project_root: path.to_string(),
-                template: super::Template::Erlang,
+                template: super::Template::Nix,
                 name: None,
                 skip_git: true,
                 skip_github: true,
@@ -264,7 +268,7 @@ fn existing_directory_with_non_generated_file() {
     let creator = super::Creator::new(
         super::NewOptions {
             project_root: path.to_string(),
-            template: super::Template::Erlang,
+            template: super::Template::Nix,
             name: None,
             skip_git: true,
             skip_github: true,
@@ -295,7 +299,7 @@ fn conflict_with_existing_files() {
         super::Creator::new(
             super::NewOptions {
                 project_root: path.to_string(),
-                template: super::Template::Erlang,
+                template: super::Template::Nix,
                 name: None,
                 skip_git: true,
                 skip_github: true,
